aefc5f5e02863a97a62f5aca1e7fce1d3fc1d1e0
tianguoyuan <603660895@qq.com>
2026-03-04 17:23:31 +0800

    h5样式修改

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,21 @@ import { navigateToInterceptor } from './interceptors/route'
 import { useAppStore } from './store/app'
 
 const appStore = useAppStore()
-onLaunch((options) => {
-  console.log('App Launch', options)
-  // 首次进入判断页面是否需要登录
+
+function setAppInfo(options: { path: string }) {
   navigateToInterceptor.invoke({ url: '/' + options.path })
   const windowInfo = uni.getWindowInfo()
   console.log('屏幕高度：', windowInfo.screenHeight)
   appStore.changeSystemScreenHeight(windowInfo.screenHeight)
+}
+onLaunch((options) => {
+  console.log('App Launch', options)
+  // 首次进入判断页面是否需要登录
+  if (!options) return
+  setAppInfo(options)
+  // #ifndef MP
+  window.addEventListener('resize', () => setAppInfo(options))
+  // #endif
 })
 onReady(() => {})
 onShow(() => {
@@ -27,6 +35,22 @@ onHide(() => {
 // #ifndef MP
 .uni-tabbar-bottom {
   display: none;
+}
+page {
+  position: relative;
+  max-width: 750px; /* 设计稿宽度 */
+  margin: 0 auto;
+}
+.tn-navbar,
+.tn-tabbar {
+  right: 0;
+  left: 0;
+  max-width: 750px; /* 设计稿宽度 */
+  margin: auto;
+}
+html,
+body {
+  background: #efefef;
 }
 // #endif
 </style>
